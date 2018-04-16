66fa808f22f85347ef20098bcb66eee8aea73ab9
alexus37 <alexlelidis@gmx.de>
2018-04-16 16:10:51 +0200

    change web page name

--- docs/demo.js
+++ docs/demo.js
@@ -2,8 +2,7 @@
 function initDemoMap(){
 
     var Esri_WorldImagery = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, ' +
-        'AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+        attribution: 'Tiles &copy; Esri'
     });
 
     var Esri_DarkGreyCanvas = L.tileLayer(
@@ -11,8 +10,7 @@ function initDemoMap(){
         "(toner-lite,$fff[difference],$fff[@23],$fff[hsl-saturation@20])/" +
         "{z}/{x}/{y}.png",
         {
-            attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, ' +
-            'NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
+            attribution: 'Tiles &copy; Esri'
         }
     );
 
